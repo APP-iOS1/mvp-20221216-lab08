4068c0b90d933ddea6a7459682c064c398d550aa
EN <12171647@inha.edu>
2022-12-22 09:29:49 +0900

    은노 필터링 에러 수정

---
+++ Touche/Touche/View/Search/FilteringContentView.swift
@@ -0,0 +1,20 @@
+//
+//  FilteringContentView.swift
+//  Touche
+//
+//  Created by mac on 2022/12/21.
+//
+
+import SwiftUI
+
+struct FilteringContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilteringContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilteringContentView()
+    }
+}
