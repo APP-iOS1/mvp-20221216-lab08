c95132f2fd5d0879df7b9dabf210a36f46473f6f
jangjonghwan <98254580+JJH0729@users.noreply.github.com>
2022-12-23 13:19:08 +0900

    클릭한향수

--- Touche/Touche/Store/ClickedStore.swift
+++ Touche/Touche/Store/ClickedStore.swift
@@ -9,7 +9,7 @@ import Foundation
 import FirebaseFirestore
 
 class ClickedStore: ObservableObject {
-    @Published var clicedStore: [Clicked] = []
+    @Published var clicedStore: [Perfume] = []
     let database = Firestore.firestore().collection("User")
     
     func fetchClickedPerfume() {
@@ -19,9 +19,13 @@ class ClickedStore: ObservableObject {
                 for document in snapshot.documents {
                     let docData = document.data()
                     let id: String = document.documentID
+                    let brand: [String] = docData["brand"] as? [String] ?? []
+                    let name: [String] = docData["name"] as? [String] ?? []
                     let imageUrl: String = docData["imageUrl"] as? String ?? ""
+                    let likedCount: Int = docData["likedCount"] as? Int ?? 0
+                    let commentsCount: Int = docData["commentsCount"] as? Int ?? 0
                     
-                    let clicked: Clicked = Clicked(id: id, imageUrl: imageUrl)
+                    let clicked: Perfume = Perfume(id: id, brand:brand, name: name, imageUrl: imageUrl, likedCount: likedCount, commentsCount: commentsCount)
                     self.clicedStore.append(clicked)
                 }
             }
@@ -31,7 +35,11 @@ class ClickedStore: ObservableObject {
     func addClickedPerfume(perfume: Perfume) {
         database.document("i5yMiGuhXTQdocwzfX4nJZf4cjg1").collection("ClickedPerfume").document(perfume.id ?? "")
             .setData([
-                "imageUrl": perfume.imageUrl ?? ""
+                "brand": perfume.brand?[0] ?? "",
+                "name": perfume.name?[0] ?? "",
+                "imageUrl": perfume.imageUrl ?? "",
+                "likedCount": perfume.likedCount ?? 0,
+                "commentsCount": perfume.commentsCount ?? 0
             ])
         
         fetchClickedPerfume()
--- Touche/Touche/View/Home/HomeView.swift
+++ Touche/Touche/View/Home/HomeView.swift
@@ -33,138 +33,175 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack(alignment: .leading) {
-                    
-                    // 배너
-                    ZStack (alignment: .leading) {
-                        HStack {
-                            Text("\n\n\n\n\n\n지금 새롭게\n추가된 향수")
-                                .font(.largeTitle)
-                                .fontWeight(.semibold)
-                                .foregroundColor(.white)
-                            Spacer()
-                        }
-                    }
-                    .padding()
-                    .background(.black)
-                    
-                    // 더보기
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("지금 프로모션 중인 향수를 확인하세요")
-                                .foregroundColor(.black)
-                            
-                            Text("더보기")
-                                .underline()
-                                .foregroundColor(.black)
-                        }
-                        Spacer()
-                        Text("닫기")
-                            .foregroundColor(.gray)
-                    }
-                    .padding()
-                    .background(Color(.gray).opacity(0.4))
-                    .padding(.top, -10)
-                    
-                    // 지금 사람들이 많이 검색한 브랜드
-                    Text("지금 사람들이 많이 검색한 브랜드")
-                        .font(.system(size: 20))
-                        .fontWeight(.semibold)
-                        .foregroundColor(.black)
-                        .padding()
-                    
-                    ForEach(mostSearchedBrands) { brand in
-                        Text(brand.brand?[0] ?? "")
-                            .font(.system(size: 25))
-                            .fontWeight(.regular)
-                            .foregroundColor(.black)
-                            .lineSpacing(4.5)
-                            .padding(.leading)
-                    }
-                    
-                    // 내가 클릭한 향수
-                    HStack {
-                        Text("최근 본 향수")
-                            .foregroundColor(.black)
-                            .font(.system(size: 20))
-                            .fontWeight(.semibold)
-                            .padding()
-                    }
-                    
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(clickedStore.clicedStore) { item in
-                                NavigationLink(value: item) {
-                                    ClickedCellView(clicked: item)
-                                }
-                                .navigationDestination(for: Clicked.self, destination: { item2 in
-                                })
-                                .padding(.leading)
+                ZStack {
+                    VStack(alignment: .leading) {
+                        
+                        // 배너
+                        ZStack (alignment: .leading) {
+                            HStack {
+                                Text("\n\n\n\n\n\n지금 새롭게\n추가된 향수")
+                                    .font(.largeTitle)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.white)
+                                Spacer()
                             }
                         }
-                    }
-                    
-                    // 코멘트 많이 달린 향수
-                    HStack {
-                        Text("지금 실시간 코멘트 많이 달린 향수")
+                        // 고정 헤더 만들기 위해서 패딩 내리기
+//                        .padding(.horizontal, 20)
+//                        .padding(.top, 100)
+                        
+                        .padding()
+                        .background(.black)
+                        
+                        // 더보기
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("지금 프로모션 중인 향수를 확인하세요")
+                                    .foregroundColor(.black)
+                                
+                                Text("더보기")
+                                    .underline()
+                                    .foregroundColor(.black)
+                            }
+                            Spacer()
+                            Text("닫기")
+                                .foregroundColor(.gray)
+                        }
+                        .padding()
+                        .background(Color(.gray).opacity(0.4))
+                        .padding(.top, -10)
+                        
+                        // 지금 사람들이 많이 검색한 브랜드
+                        Text("지금 사람들이 많이 검색한 브랜드")
                             .font(.system(size: 20))
                             .fontWeight(.semibold)
                             .foregroundColor(.black)
-                    }
-                    .padding()
-                    .padding(.top, 30)
-                    
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(manyCommentsTop) { item in
-                                NavigationLink(value: item) {
-                                    LotCommentsCellView(perfume: item)
-                                }
-                                .navigationDestination(for: Perfume.self, destination: { item in
-                                    DetailView(perfume: item, perfumeUid: item.id ?? "")
-                                })
-                                .simultaneousGesture(
-                                    TapGesture()
-                                        .onEnded({ _ in
-                                            clickedStore.addClickedPerfume(perfume: item)
-                                        })
-                                )
+                            .padding()
+                        
+                        ForEach(mostSearchedBrands) { brand in
+                            Text(brand.brand?[0] ?? "")
+                                .font(.system(size: 25))
+                                .fontWeight(.regular)
+                                .foregroundColor(.black)
+                                .lineSpacing(4.5)
                                 .padding(.leading)
+                        }
+                        
+                        // 내가 클릭한 향수
+                        HStack {
+                            Text("최근 본 향수")
+                                .foregroundColor(.black)
+                                .font(.system(size: 20))
+                                .fontWeight(.semibold)
+                                .padding()
+                        }
+                        
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(clickedStore.clicedStore) { item in
+                                    ClickedCellView(perfume: item)
+                                        .padding(.leading)
+                                }
                             }
                         }
                         
+                        // 코멘트 많이 달린 향수
                         HStack {
-                            ForEach(manyCommentsBottom) { item in
-                                NavigationLink(value: item) {
-                                    LotCommentsCellView2(perfume: item)
+                            Text("지금 실시간 코멘트 많이 달린 향수")
+                                .font(.system(size: 20))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                        }
+                        .padding()
+                        .padding(.top, 30)
+                        
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(manyCommentsTop) { item in
+                                    NavigationLink(value: item) {
+                                        LotCommentsCellView(perfume: item)
+                                    }
+                                    .navigationDestination(for: Perfume.self, destination: { item in
+                                        DetailView(perfume: item, perfumeUid: item.id ?? "")
+                                    })
+                                    .simultaneousGesture(
+                                        TapGesture()
+                                            .onEnded({ _ in
+                                                clickedStore.addClickedPerfume(perfume: item)
+                                            })
+                                    )
+                                    .padding(.leading)
                                 }
-                                .navigationDestination(for: Perfume.self, destination: { item in
-                                    DetailView(perfume: item, perfumeUid: item.id ?? "")
-                                })
-                                .simultaneousGesture(
-                                    TapGesture()
-                                        .onEnded({ _ in
-                                            clickedStore.addClickedPerfume(perfume: item)
-                                        })
-                                )
-                                .padding(.leading)
+                            }
+                            
+                            HStack {
+                                ForEach(manyCommentsBottom) { item in
+                                    NavigationLink(value: item) {
+                                        LotCommentsCellView2(perfume: item)
+                                    }
+                                    .navigationDestination(for: Perfume.self, destination: { item in
+                                        DetailView(perfume: item, perfumeUid: item.id ?? "")
+                                    })
+                                    .simultaneousGesture(
+                                        TapGesture()
+                                            .onEnded({ _ in
+                                                clickedStore.addClickedPerfume(perfume: item)
+                                            })
+                                    )
+                                    .padding(.leading)
+                                }
                             }
                         }
                     }
+//                    GeometryReader { gr in
+//                        VStack {
+//                            Rectangle()
+//                                .fill(Color.white)
+//                                .frame(height:
+//                                        self.calculateHeight(minHeight: 100,
+//                                                             maxHeight: 100,
+//                                                             yOffset: gr.frame(in: .global).origin.y))
+//                                .overlay(
+//                                    HStack(spacing: 10) {
+//                                        Spacer()
+//                                        Image(systemName: "magnifyingglass")
+//                                        Image(systemName: "bell")
+//                                        Image(systemName: "bag")
+//                                    }
+//                                        .padding()
+//                                        .font(.system(size: 20, weight: .medium))
+//                                        .foregroundColor(.black)
+//                                        .opacity(0.8))
+//                                .offset(y: gr.frame(in: .global).origin.y < 0
+//                                        ? abs(gr.frame(in: .global).origin.y)
+//                                        : -gr.frame(in: .global).origin.y)
+//                            Spacer() // 헤더를 맨 위로 푸시
+//                        }
+//                    }
                 }
             }
+//            .edgesIgnoringSafeArea(.vertical)
         }
         .onAppear {
             perfumeStore.fetchPerfume()
             clickedStore.fetchClickedPerfume()
         }
     }
+    
+    func calculateHeight(minHeight: CGFloat, maxHeight: CGFloat, yOffset: CGFloat) -> CGFloat {
+        if maxHeight + yOffset < minHeight {
+            return minHeight
+        }
+        return maxHeight + yOffset
+    }
 }
-
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
 }
-
+//
+//magnifyingglass
+//bell
+//bag
